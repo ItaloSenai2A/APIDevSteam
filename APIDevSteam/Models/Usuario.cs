eea6f8747a7f65229d2bd53579c1d2e5b4f90b51
Emanuelly <emanuellydssenai@gmail.com>
2025-04-30 09:06:22 -0300

    Adicionada propriedades da classe Usuario

--- APIDevSteam/Models/Usuario.cs
+++ APIDevSteam/Models/Usuario.cs
@@ -4,10 +4,11 @@ namespace APIDevSteam.Models
 {
     public class Usuario : IdentityUser
     {
-
         public string? NomeCompleto { get; set; }
         public DateOnly DataNascimento { get; set; }
-
+        public string? Cpf { get; set; }
+        public string? ChavePix { get; set; }
+        public string? NumeroCartao { get; set; }
 
         public Usuario() : base()
         {
